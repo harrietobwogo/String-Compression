d048fc57d10355c1e22939bb7ba221365d30e174
Harriet Obwogo <harrietobwogo@outlook.com>
2021-09-02 11:02:49 +0000

    Added test data

--- main.cs
+++ main.cs
@@ -3,8 +3,19 @@ using System;
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
+		char[] test1=new char[]{'a','b','b','b','b','b','b','b','b','b','b','b','b'};
+		char[] test2=new char[]{'a','b','b','b','b','c','c','c'};
+		char[] test3=new char[]{'a'};
+		char[] test4=new char[]{'a','a','b','b','b','a','a'};
+
+
+		Console.WriteLine(Compress(test1));
+		Console.WriteLine(Compress(test2));
+		Console.WriteLine(Compress(test3));
+		Console.WriteLine(Compress(test4));
+
   }
-	public int Compress(char[] chars) {
+	public static int Compress(char[] chars) {
     if(chars.Length<2){
       return chars.Length;
     }
